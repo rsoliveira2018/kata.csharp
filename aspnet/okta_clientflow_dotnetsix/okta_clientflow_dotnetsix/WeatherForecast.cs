8e1baf86b6b685f77728549e099edbfc377de2f5
Rafael Silvestre De Oliveira <rafael.silvestre@ambevtech.com.br>
2022-09-01 10:55:15 -0300

    webapi dotnetsix clientflow project created

---
+++ aspnet/okta_clientflow_dotnetsix/okta_clientflow_dotnetsix/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace okta_clientflow_dotnetsix
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
